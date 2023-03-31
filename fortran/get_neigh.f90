d0fa6cddc8bc943972d9da430e3bcfb18983157b
zhangylch <ylzustc@mail.ustc.edu.cn>
2023-03-31 12:30:07 +0200

    neigh_list

--- fortran/get_neigh.f90
+++ fortran/get_neigh.f90
@@ -1,11 +1,16 @@
 subroutine get_neigh(coor,atomindex,shifts)
      use initmod
      implicit none
-     integer(kind=intype) :: numatom,num,iatom,i,j,l,i1,i2,i3
+     integer(kind=intype) :: num,iatom,ninit,scutnum,i,j,l,i1,i2,i3
      integer(kind=intype) :: sca(3),boundary(2,3)
-     real(kind=typenum) :: oriminv(3),tmp(3),tmp1(3),boundary(3,3)
+     integer(kind=intype) :: index_numrs(2,int(numatom*dier_3/volume*expand_coeff),rangebox(1),rangebox(2),rangebox(3))
+     integer(kind=intype) :: index_rs(rangebox(1),rangebox(2),rangebox(3))
+     real(kind=typenum) :: tmp
+     real(kind=typenum) :: oriminv(3),tmp1(3)
      real(kind=typenum) :: coor(3,numatom),fcoor(3,numatom),imageatom(3,numatom,length)
-     real(kind=typenum) :: shifts(3,max_neigh*numatom),atomindex(2,max_neigh*numatom)
+     real(kind=typenum) :: shifts(3,maxneigh),atomindex(2,maxneigh)
+     ! to calculate the number of atoms in each box with its sidelength as dier
+     ! nbox=dier^3/matrix(1,1)*matrix(2,2)*matrix(3,3)*numatom
 !f2py integer(kind=intype),intent(in) :: numatom
 !f2py real(kind=typenum),intent(in),check(0==0) :: coor
 !f2py integer(kind=intype),intent(out),check(0==0) :: atomindex
@@ -15,8 +20,8 @@ subroutine get_neigh(coor,atomindex,shifts)
        oriminv=coor(:,1)
        do iatom=2,numatom
          sca=nint(fcoor(:,iatom)-fcoor(:,1))
-         coor(:,iatom)=coor(:,iatom)-sca(1)*scalmatrix(:,1)-sca(2)*scalmatrix(:,2)-sca(3)*scalmatrix(:,3)
-         do j=1,atomdim
+         coor(:,iatom)=coor(:,iatom)-sca(1)*matrix(:,1)-sca(2)*matrix(:,2)-sca(3)*matrix(:,3)
+         do j=1,3
            if(coor(j,iatom)<oriminv(j)) oriminv(j)=coor(j,iatom)
          end do
        end do
@@ -30,14 +35,15 @@ subroutine get_neigh(coor,atomindex,shifts)
            imageatom(:,iatom,l)=coor(:,iatom)+shiftvalue(:,l)
          end do
        end do
-
+       index_rs=0
        do l=1,length
          do iatom=1,numatom
-           if(imageatom(1,iatom,l)>0d0 .and. imageatom(1,iatom,l)<rangecoor(1).and. imageatom(2,iatom,l)>0d0 .and.
-           imageatom(2,iatom,l)<rangecoor(2)  &
+           if(imageatom(1,iatom,l)>0d0 .and. imageatom(1,iatom,l)<rangecoor(1).and. &
+           imageatom(2,iatom,l)>0d0 .and. imageatom(2,iatom,l)<rangecoor(2)  &
            .and. imageatom(3,iatom,l)>0d0 .and. imageatom(3,iatom,l)<rangecoor(3)) then
              sca=ceiling(imageatom(:,iatom,l)/dier)
-             index_numrs(:,sca(1),sca(2),sca(3))=[iatom,l] 
+             index_rs(sca(1),sca(2),sca(3))=index_rs(sca(1),sca(2),sca(3))+1
+             index_numrs(:,index_rs(sca(1),sca(2),sca(3)),sca(1),sca(2),sca(3))=[iatom,l] 
            end if
          end do
        end do
@@ -47,7 +53,7 @@ subroutine get_neigh(coor,atomindex,shifts)
          sca=ceiling(coor(:,iatom)/dier)
          imageatom(:,iatom,ninit)=100.0
          ninit=(length+1)/2
-         dire(:,natom,ninit)=100d0
+         imageatom(:,iatom,ninit)=100d0
          do i=1,3
            boundary(1,i)=max(1,sca(i)-interaction)
            boundary(2,i)=min(rangebox(i),sca(i)+interaction)
@@ -55,19 +61,21 @@ subroutine get_neigh(coor,atomindex,shifts)
          do i3=boundary(1,3),boundary(2,3)
            do i2=boundary(1,2),boundary(2,2)
              do i1=boundary(1,1),boundary(2,1)
-               j=index_numrs(1,i,i1,i2,i3)
-               l=index_numrs(2,i,i1,i2,i3)
-               tmp1=imageatom(:,j,l)-coor(:,iatom)
-               tmp=dot_product(tmp1,tmp1)
-               if(tmp<=rcsq) then
-                 atomindex(:,scutnum)=[iatom-1,j-1]
-                 shifts(:,scutnum)=shiftvalue(:,l)
-                 scutnum=scutnum+1
-               end if
+               do i=1,index_rs(i1,i2,i3)
+                 j=index_numrs(1,i,i1,i2,i3)
+                 l=index_numrs(2,i,i1,i2,i3)
+                 tmp1=imageatom(:,j,l)-coor(:,iatom)
+                 tmp=dot_product(tmp1,tmp1)
+                 if(tmp<=rcsq) then
+                   atomindex(:,scutnum)=[iatom-1,j-1]
+                   shifts(:,scutnum)=shiftvalue(:,l)
+                   scutnum=scutnum+1
+                 end if
+               end do
              end do
            end do
          end do
-         imageatom(:,natom,ninit)=coor(:,j)
+         imageatom(:,iatom,ninit)=coor(:,j)
        end do
      return
 end subroutine
--- fortran/init_dealloc.f90
+++ fortran/init_dealloc.f90
@@ -1,41 +1,38 @@
 module initmod
      implicit none
      integer(kind=4),parameter :: intype=4,typenum=8
-     real(kind=typenum),parameter :: dier=0.25 ! "dier" is the side length of the box used in cell-linked list algorithm. This value must be small enough to ensure each box only have one atom at most.
-     integer(kind=intype) :: interaction,numatom,length,max_neigh
+     real(kind=typenum),parameter :: dier=0.25,dier_3=dier*dier*dier ! "dier" is the side length of the box used in cell-linked 
+     real(kind=typenum),parameter :: expand_coeff=1.2
+     integer(kind=intype) :: interaction,numatom,length,maxneigh
      integer(kind=intype) :: nimage(3),rangebox(3)
-     integer(kind=intype),allocatable :: index_numrs(:,:,:,:)
-     real(kind=typenum) :: rc,rcsq
+     real(kind=typenum) :: rc,rcsq,volume
      real(kind=typenum) :: matrix(3,3),inv_matrix(3,3),rangecoor(3)
-     real(kind=typenum),allocatable :: imageatom(:,:,:),shiftvalue(:,:)
+     real(kind=typenum),allocatable :: shiftvalue(:,:)
 end module
 
-subroutine init_neigh(in_numatom,in_max_neigh,in_rc,cell)
+subroutine init_neigh(in_numatom,in_rc,cell,in_maxneigh)
      use initmod
      implicit none
-     integer(kind=intype) :: in_max_neigh,in_numatom
+     integer(kind=intype) :: in_numatom,in_maxneigh
+     integer(kind=intype) :: i,j,k,l
      real(kind=typenum) :: in_rc,in_dier
      real(kind=typenum) :: s1,s2,rlen1,rlen2
      real(kind=typenum) :: cell(3,3)
-     real(kind=typenum) :: tmp(3),maxd(3),mind(3),vel1(3,3),vel2(3,3)
-!f2py integer(kind=intype),intent(in) :: in_max_neigh,in_max_neigh
+     real(kind=typenum) :: tmp(3),maxd(3),mind(3),vec1(3,3),vec2(3,3)
+!f2py real(kind=intype),intent(in) :: max_neigh
 !f2py real(kind=typenum),intent(in) :: in_rc,in_dier,cell
        numatom=in_numatom
-       max_neigh=in_max_neigh
+       maxneigh=in_maxneigh
        rc=in_rc
-       dier=in_dier
        rcsq=rc*rc
        interaction=ceiling(rc/dier)
        matrix=cell
        call inverse_matrix(matrix,inv_matrix)
 !Note that the fortran store the array with the column first, so the lattice parameters is the transpose of the its realistic shape
-       s1=abs(matrix(1,1)*matrix(2,2))
-       s2=abs(matrix(1,1)*matrix(3,3))
-       rlen1=sqrt(dot_product(matrix(:,1),matrix(:,1)))
-       rlen2=sqrt(dot_product(matrix(:,2),matrix(:,2)))
-       tmp(1)=s1/rlen2
-       tmp(2)=s1/rlen1
-       tmp(3)=s2/rlen1
+       tmp(1)=matrix(1,1)
+       tmp(2)=matrix(2,2)
+       tmp(3)=matrix(3,3)
+       volume=tmp(1)*tmp(2)*tmp(3)
        nimage=ceiling(rc/abs(tmp))
        length=(2*nimage(1)+1)*(2*nimage(2)+1)*(2*nimage(3)+1)
        call inverse_matrix(matrix,inv_matrix)
@@ -47,8 +44,6 @@ subroutine init_neigh(in_numatom,in_max_neigh,in_rc,cell)
        rangebox=ceiling(rangecoor/dier)
 
 ! allocate the array
-       allocate(imageatom(3,numatom,length)) 
-       allocate(index_numrs(2,rangebox(1),rangebox(2),rangebox(3)))
        allocate(shiftvalue(3,length))
 ! obatin image 
        vec2(:,1)=-matrix(:,1)*nimage(1)
@@ -75,8 +70,6 @@ end subroutine
 subroutine deallocate_all()
      use initmod
      implicit none
-       deallocate(imageatom)
-       deallocate(index_numrs)
        deallocate(shiftvalue)
      return
 end subroutine
